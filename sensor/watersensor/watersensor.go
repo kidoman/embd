e9d5fc03efe783ada0aec558aaa69c077f7419e1
Kashyap Kopparam <kashyapkopparam@gmail.com>
2013-12-23 16:48:00 +0530

    Water sensor Packaged

--- sensor/watersensor/watersensor.go
+++ sensor/watersensor/watersensor.go
@@ -1,3 +1,4 @@
+// Package watersensor allows interfacing with the water sensor
 package watersensor
 
 import (
@@ -8,43 +9,48 @@ import (
 
 type watersensor struct {
     waterPinNumber int
-        waterPin rpio.Pin
-        initialized bool
-        mu *sync.RWMutex
-        debug bool
+    waterPin rpio.Pin
+
+    initialized bool
+    mu *sync.RWMutex
+
+    debug bool
 }
 
+// WaterSensor implements access to a water sensor
 type WaterSensor interface {
+    // IsWet determines if there is water present on the sensor
     IsWet() (b bool,err error)
 }
 
+// New creates a new WaterSensor interface
 func New(pinNumber int) WaterSensor {
     return &watersensor{waterPinNumber: pinNumber, mu: new(sync.RWMutex)}
 }
 
 func (d *watersensor) Setup() (err error) {
     d.mu.RLock()
-        if d.initialized {
-            d.mu.RUnlock()
-                return
-        }
+    if d.initialized {
+        d.mu.RUnlock()
+        return
+    }
     d.mu.RUnlock()
 
-        d.mu.Lock()
-        defer d.mu.Unlock()
+    d.mu.Lock()
+    defer d.mu.Unlock()
 
-        if err = rpio.Open(); err != nil {
-            return
-        }
+    if err = rpio.Open(); err != nil {
+        return
+    }
+
     d.waterPin = rpio.Pin(d.waterPinNumber)
+    d.waterPin.Input()
+    d.initialized = true
 
-        d.waterPin.Input()
-
-        d.initialized = true
-
-        return nil
+    return nil
 }
 
+// IsWet determines if there is water present on the sensor
 func (d *watersensor) IsWet() (b bool, err error) {
     if err = d.Setup(); err != nil {
         return
@@ -54,6 +60,7 @@ func (d *watersensor) IsWet() (b bool, err error) {
         log.Print("Getting reading")
     }
 
+    // Read the pin value of the sensor
     if d.waterPin.Read() == rpio.High {
         b=true
     } else {
