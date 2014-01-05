36c7f3a536f0fea631d8c34878b1fd6dda68db1e
Karan Misra <kidoman@gmail.com>
2014-01-06 02:34:19 +0530

    add doc

--- util/map.go
+++ util/map.go
@@ -1,5 +1,12 @@
+// Package util contains utility functions.
 package util
 
+// Map re-maps a number from one range to another.
+//
+// Example:
+//
+//	val := Map(angle, 0, 180, 1000, 2000)
+//
 func Map(x, inmin, inmax, outmin, outmax int64) int64 {
 	return (x-inmin)*(outmax-outmin)/(inmax-inmin) + outmin
 }
