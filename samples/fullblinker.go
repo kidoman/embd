946aa2694f7886f1e03b8a7764ba29d9f018a0af
Karan Misra <kidoman@gmail.com>
2014-04-11 10:51:29 +0530

    samples: full blinker rpi example
    this is a more complete version of the simple blinker. this cleans up
    after itself

---
+++ samples/fullblinker.go
@@ -0,0 +1,50 @@
+// +build ignore
+
+// LED blinker, works OOTB on a RPi.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/kidoman/embd"
+	_ "github.com/kidoman/embd/host/rpi"
+)
+
+func main() {
+	flag.Parse()
+
+	if err := embd.InitLED(); err != nil {
+		panic(err)
+	}
+	defer embd.CloseLED()
+
+	led, err := embd.NewLED(0)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		led.Off()
+		led.Close()
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, os.Kill)
+	defer signal.Stop(quit)
+
+	for {
+		select {
+		case <-time.After(250 * time.Millisecond):
+			if err := led.Toggle(); err != nil {
+				panic(err)
+			}
+			fmt.Printf("Toggled\n")
+		case <-quit:
+			return
+		}
+	}
+}
